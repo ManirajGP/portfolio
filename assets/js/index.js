3462f44db077d7ae4ad03b4aeec0dbbed3926487
Maniraj G Parthasarathy <maniraj1893@gmail.com>
2023-12-13 18:16:25 -0500

    Move contents of Public/assets to ./assets

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// Initialize Rellax Js (for Home Screen Only)
+document.addEventListener('DOMContentLoaded', function () {
+    // Initialize Rellax
+    var rellax = new Rellax('.rellax', {
+        vertical: true,   // Disable vertical scrolling
+    });
+  });
